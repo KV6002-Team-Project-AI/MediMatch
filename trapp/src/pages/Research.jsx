b979961e357fc6aeb72d96e38351816b531b8c9d
Syed W. Quadri <swq1999@gmail.com>
2024-03-13 21:10:55 +0000

    menu fixed

--- trapp/src/pages/Research.jsx
+++ trapp/src/pages/Research.jsx
@@ -5,7 +5,6 @@ function Research() {
     return (
         <>
             <div className="mx-3 my-20">
-                
                 <div className="grid grid-col pb-4">
                     <div className="flex justify-center items-center bg-white transition duration-500 ease-in-out shadow-md hover:bg-gray-100 rounded-2xl hover:shadow-2xl">
                         <div className='w-full px-1'>

79ebb8143bcb3fecb9289092329a049952a267ab
moetri17 <mohamedetri667@gmail.com>
2024-03-13 22:31:47 +0000

    Tinder.jsx

--- trapp/src/pages/Tinder.jsx
+++ trapp/src/pages/Tinder.jsx
@@ -1,17 +1,29 @@
+import React from 'react';
 
 
-
-function Tinder() {
+const Tinder = () => {
 
   return (
       <>
-          <div className="mx-3 my-20">
-              <div className="flex flex-col items-center bg-white transition duration-500 ease-in-out shadow-md hover:bg-gray-100 rounded-2xl hover:shadow-2xl p-6">
-                  
-              </div>
+          <div className="mx-3 my-20 flex justify-center">
+              <select className="bg-white transition duration-500 ease-in-out shadow-md hover:bg-gray-100 rounded-2xl hover:shadow-2xl p-2">
+                <option value="Study 1">Study 1</option>
+                <option value="Study 2">Study 2</option>
+                <option value="Study 3">Study 3</option>
+                <option value="Study 4">Study 4</option>
+              </select>
+          </div>
+          <div className="mx-10 my-20 flex justify-center">
+            <div className="bg-white transition duration-500 ease-in-out shadow-md hover:bg-gray-100 rounded-2xl hover:shadow-2xl p-2">
+            </div>
           </div>
       </>
   )
+
 }
 
+<<<<<<< Updated upstream
 export default Tinder
+=======
+export default Tinder;
+>>>>>>> Stashed changes

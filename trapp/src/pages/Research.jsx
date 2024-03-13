8aa5f0223ea23e91714c390ac5aa3559886b0778
Syed W. Quadri <swq1999@gmail.com>
2024-03-13 17:21:45 +0000

    titles

--- trapp/src/pages/Research.jsx
+++ trapp/src/pages/Research.jsx
@@ -7,7 +7,7 @@ function Research() {
         <>
             <div className="mx-3 my-20">
                 <div className="flex flex-col items-center bg-white transition duration-500 ease-in-out shadow-md hover:bg-gray-100 rounded-2xl hover:shadow-2xl p-6">
-                    
+                    <h1 className="text-3xl font-semibold text-center">Studies</h1>
                 </div>
             </div>
         </>

96b8960a3774bfbb74197ff0de3172deedd1b5f4
Syed W. Quadri <swq1999@gmail.com>
2024-03-13 21:01:31 +0000

    Update Research.jsx

--- trapp/src/pages/Research.jsx
+++ trapp/src/pages/Research.jsx
@@ -8,7 +8,7 @@ function Research() {
                 
                 <div className="grid grid-col pb-4">
                     <div className="flex justify-center items-center bg-white transition duration-500 ease-in-out shadow-md hover:bg-gray-100 rounded-2xl hover:shadow-2xl">
-                        <div className='w-full px-1 md:px-2'>
+                        <div className='w-full px-1'>
                             <div className='flex p-2 gap-2 justify-between'> 
                                 <div className='flex items-center'>
                                     <input type="text" placeholder="Search..." className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500" />

cf71b1372b2d1f55b32846ae6174e84ea77efd4d
Syed W. Quadri <swq1999@gmail.com>
2024-03-13 20:29:39 +0000

    Update Research.jsx

--- trapp/src/pages/Research.jsx
+++ trapp/src/pages/Research.jsx
@@ -5,6 +5,23 @@ function Research() {
     return (
         <>
             <div className="mx-3 my-20">
+                
+                <div className="grid grid-col pb-4">
+                    <div className="flex justify-center items-center bg-white transition duration-500 ease-in-out shadow-md hover:bg-gray-100 rounded-2xl hover:shadow-2xl">
+                        <div className='w-full px-1 md:px-2'>
+                            <div className='flex p-2 gap-2 justify-between'> 
+                                <div className='flex items-center'>
+                                    <input type="text" placeholder="Search..." className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500" />
+                                </div>
+                                <div className='flex items-center'>
+                                    <button className="bg-green-600 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded-lg shadowtransition duration-300 ease-in-out transform hover:-translate-y-0.5">
+                                        Add Research
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
                     <div className="flex justify-center items-center bg-white transition duration-500 ease-in-out shadow-md hover:bg-gray-100 rounded-2xl hover:shadow-2xl">
                         <div className='w-full px-1 md:px-2'>
@@ -28,10 +45,10 @@ function Research() {
                                 </div>
                             </div>
                             <div className='flex mx-2 mb-2 text-white gap-2 text-center'>
-                                <div className='w-full bg-blue-600 p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-blue-800 transform hover:-translate-y-0.5'>
+                                <div className='w-full bg-blue-500 p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-blue-600 transform hover:-translate-y-0.5'>
                                     Edit
                                 </div>
-                                <div className='w-full bg-red-600  p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-red-800 transform hover:-translate-y-0.5'>
+                                <div className='w-full bg-red-500  p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-red-800 transform hover:-translate-y-0.5'>
                                     Delete
                                 </div>
                             </div>
@@ -59,15 +76,16 @@ function Research() {
                                 </div>
                             </div>
                             <div className='flex mx-2 mb-2 text-white gap-2 text-center'>
-                                <div className='w-full bg-blue-600 p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-blue-800 transform hover:-translate-y-0.5'>
+                                <div className='w-full bg-blue-500 p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-blue-600 transform hover:-translate-y-0.5'>
                                     Edit
                                 </div>
-                                <div className='w-full bg-red-600  p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-red-800 transform hover:-translate-y-0.5'>
+                                <div className='w-full bg-red-500  p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-red-800 transform hover:-translate-y-0.5'>
                                     Delete
                                 </div>
                             </div>
                         </div>
                     </div>
+
                 </div>
             </div>
         </>

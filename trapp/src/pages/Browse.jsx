e238a93a9d1a1b4f5e1e6fe905e46b42d8980df6
Syed W. Quadri <swq1999@gmail.com>
2024-03-14 15:14:25 +0000

    Update Browse.jsx

--- trapp/src/pages/Browse.jsx
+++ trapp/src/pages/Browse.jsx
@@ -19,13 +19,13 @@ function Browse() {
             <div className="mx-3 my-20">
             <div className="grid grid-col pb-4">
                 <div className="flex justify-center items-center bg-white transition duration-500 ease-in-out shadow-md hover:bg-gray-100 rounded-2xl hover:shadow-2xl">
-                    <div className='w-full px-1'>
-                        <div className='flex p-2 gap-2 justify-between'> 
-                            <div className='flex items-center'>
-                                <input type="text" placeholder="Search..." className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500" />
+                    <div className='px-1 w-full'>
+                        <div className='sm:flex p-2 gap-2 justify-between md:flex-row'> 
+                            <div className='flex items-center pb-2 sm:pb-0'>
+                                <input type="text" placeholder="Search..." className="px-4 py-2 border w-full  border-gray-300 rounded-lg focus:outline-none focus:border-blue-300" />
                             </div>
                             <div className='flex items-start'>
-                                <select className="font-semibold py-2 px-2 rounded-lg">
+                                <select className="font-semibold py-2 px-2 rounded-lg w-full">
                                     <option value="addResearch">Select Field</option>
                                     {researchFields.map((field, index) => (
                                         <option key={index} value={field}>{field}</option>

cb778299cf4eac49a69a20a510c4ecf89e0630d6
moetri17 <mohamedetri667@gmail.com>
2024-03-14 16:16:02 +0000

    Tinder.jsx

--- trapp/src/pages/Tinder.jsx
+++ trapp/src/pages/Tinder.jsx
@@ -3,9 +3,9 @@ import ProfileCard from '../components/ProfileCard';
 
 const Tinder = () => {
   return (
-      <>
-      <div className='flex-col'>
-        <div className="mx-14 mt-20 ">
+    <>
+      <div className='flex flex-col items-center justify-center min-h-screen mx-10 mb-20'>
+        <div className="w-full max-w-auto mx-auto">
           <div className="flex justify-center p-2 items-center bg-white transition duration-500 ease-in-out shadow-md hover:bg-gray-100 rounded-2xl hover:shadow-2xl">
             <select className="font-semibold py-2 px-2 rounded-lg">
               <option value="Study 1">Study 1</option>
@@ -14,27 +14,24 @@ const Tinder = () => {
           </div>
         </div>
 
+        <ProfileCard/>
 
-          <ProfileCard />     
-
-            
-          <div className="flex-none mx-14 justify-center items-center">
-            <div className='flex text-white gap-2 text-center'>
-                <div className='w-full bg-red-600  p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-red-800 transform hover:-translate-y-0.5'>
-                    Reject
-                </div>
-                <div className='w-full bg-cyan-600 p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-cyan-800 transform hover:-translate-y-0.5'>
-                    Refresh
-                </div>
-                <div className='w-full bg-green-600 p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-green-800 transform hover:-translate-y-0.5'>
-                    Accept
-                </div>
+        <div className="w-full max-w-auto mx-auto">
+          <div className='flex text-white gap-2 text-center'>
+            <div className='w-full bg-red-600 p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-red-800 transform hover:-translate-y-0.5'>
+              Reject
+            </div>
+            <div className='w-full bg-cyan-600 p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-cyan-800 transform hover:-translate-y-0.5'>
+              Refresh
+            </div>
+            <div className='w-full bg-green-600 p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-green-800 transform hover:-translate-y-0.5'>
+              Accept
             </div>
           </div>
+        </div>
       </div>
-      </>
+    </>
   )
-
 }
 
 export default Tinder;

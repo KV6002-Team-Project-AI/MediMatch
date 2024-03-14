1bbffd5bfb2d5b0f5f283d78449b0c30898519fa
moetri17 <mohamedetri667@gmail.com>
2024-03-14 15:16:25 +0000

    Update ProfileCard.jsx

--- trapp/src/components/ProfileCard.jsx
+++ trapp/src/components/ProfileCard.jsx
@@ -8,15 +8,15 @@ const ProfileCard = () => {
     const interests = ['Football', 'Swimming', 'Reading'];
   
     return (
-      <div className='mt-5 mb-5 mx-14'>
+      <div className='mt-5 mb-5 mx-10'>
         <div className="flex flex-col justify-center p-2 items-center bg-white transition duration-500 ease-in-out shadow-md hover:bg-gray-100 rounded-2xl hover:shadow-2xl relative">
           <img src={summarise} alt="summarise" className=" w-7 h-7 hover:bg-gray-200 transition duration-300 ease-in-out transform hover:-translate-y-0.5 absolute top-2 left-2" />
           <img src={infoLogo} alt="info" className="w-7 h-7 hover:bg-gray-200 rounded-xl transition duration-300 ease-in-out transform hover:-translate-y-0.5 absolute top-2 right-2"/>
-          <img src={profilePic} alt="Person" className="w-40 h-40 rounded-full"/>
+          <img src={profilePic} alt="Person" className="w-20 h-20 rounded-full"/>
           <p className="font-medium text-lg text-gray-800">John Doe</p>
           <p className='text-center font-normal mt-3'>Welcome to my natural habitat, where I can speak no more than this.</p>
   
-          <hr className="border-gray-400 w-full mt-3" />
+          <hr className="border-gray-400 w-full mt-0" />
   
           <div className="font-semibold justify-center mt-3 mb-3 underline underline-offset-2">Features: </div>
           <div className="flex gap-2">
@@ -27,7 +27,7 @@ const ProfileCard = () => {
             ))}
           </div>
   
-          <hr className="border-gray-400 w-full mt-5" />
+          <hr className="border-gray-400 w-full mt-3" />
   
           <div className="font-semibold justify-center mt-3 mb-3 underline underline-offset-2">Interests: </div>
           <div className="flex gap-2">

9f1f22f33e2753a20066f6fcb18b947109e1a94c
moetri17 <mohamedetri667@gmail.com>
2024-03-16 01:14:03 +0000

    Tinder.jsx

--- trapp/src/components/ProfileCard.jsx
+++ trapp/src/components/ProfileCard.jsx
@@ -38,7 +38,7 @@ const ProfileCard = () => {
               ))}
             </div>
           </div>
-        </div>
+        </div>      
     );
   };
   
--- trapp/src/pages/Tinder.jsx
+++ trapp/src/pages/Tinder.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProfileCard from '../components/ProfileCard';
 
 const Tinder = () => {
+  // State to manage the background color
+  const [AcceptColor, setAcceptColor] = useState('');
+  const [RejectColor, setRejectColor] = useState('');
+
+  // Function to change the background color temporarily
+  const handleAcceptClick = () => {
+    setAcceptColor('bg-green-400'); // Set the background color to green
+    setTimeout(() => setAcceptColor(''), 750); // Reset the background color after 2 seconds
+  };
+
+  
+  const handleRejectClick = () => {
+    setRejectColor('bg-red-400'); // Set the background color to green
+    setTimeout(() => setRejectColor(''), 750); // Reset the background color after 2 seconds
+  };
+
+
   return (
     <>
-      <div className='flex flex-col items-center justify-center min-h-screen mx-10 mb-20'>
-        <div className="w-full max-w-auto mx-auto">
+    <div className={`${AcceptColor || RejectColor} flex flex-col`}>
+      <div className='my-20 items-center justify-center mx-3'>
+        <div className="w-full h-full max-w-auto mx-auto">
           <div className="flex justify-center p-2 items-center bg-white transition duration-500 ease-in-out shadow-md hover:bg-gray-100 rounded-2xl hover:shadow-2xl">
             <select className="font-semibold py-2 px-2 rounded-lg">
               <option value="Study 1">Study 1</option>
@@ -18,20 +36,21 @@ const Tinder = () => {
 
         <div className="w-full max-w-auto mx-auto">
           <div className='flex text-white gap-2 text-center'>
-            <div className='w-full bg-red-600 p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-red-800 transform hover:-translate-y-0.5'>
+            <button onClick={handleRejectClick} className='w-full bg-red-600 p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-red-800 transform hover:-translate-y-0.5'>
               Reject
-            </div>
-            <div className='w-full bg-cyan-600 p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-cyan-800 transform hover:-translate-y-0.5'>
+            </button>
+            <button className='w-full bg-cyan-600 p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-cyan-800 transform hover:-translate-y-0.5'>
               Refresh
-            </div>
-            <div className='w-full bg-green-600 p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-green-800 transform hover:-translate-y-0.5'>
+            </button>
+            <button onClick={handleAcceptClick} className='w-full bg-green-600 p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-green-800 transform hover:-translate-y-0.5'>
               Accept
-            </div>
+            </button>
           </div>
         </div>
       </div>
+    </div>
     </>
-  )
-}
+  );
+};
 
 export default Tinder;

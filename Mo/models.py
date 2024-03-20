5217c72ae01d808f23498ccce11717c7f17e42a8
Jed Bywater <jedbywater@gmail.com>
2024-03-20 05:57:34 +0000

    db refesh

--- Mo/models.py
+++ Mo/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from ResearchSwipe.models import Recruitee
-<<<<<<< Updated upstream
 from Syed.models import Study
 
 class Matches(models.Model):
@@ -26,18 +25,3 @@ class Matches(models.Model):
         help_text='Accepted/Rejected status of the study'
     )
 
-    class Meta:
-        unique_together = ('study', 'recruitee')
-=======
-from Syed.models import Study 
-
-class Matches(models.Model):
-    match_id = models.AutoField(primary_key=True)
-    study = models.ForeignKey(Study, on_delete=models.CASCADE, related_name='interactions', null=True, blank=True)
-    recruitee = models.ForeignKey(Recruitee, on_delete=models.CASCADE, related_name='interactions')
-    recruitee_status = models.BooleanField(default=False, help_text='True for accepted, False for rejected')
-    study_status = models.BooleanField(default=False, help_text='True for accepted, False for rejected')
-    # Prevent duplicate interactions for simplicity
-    class Meta:
-        unique_together = ('study', 'recruitee')
->>>>>>> Stashed changes

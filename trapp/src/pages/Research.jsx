627623fba0de0c36908170784e501d867e78fdb5
Syed W. Quadri <swq1999@gmail.com>
2024-03-22 16:19:52 +0000

    form fully functional

--- trapp/src/pages/Research.jsx
+++ trapp/src/pages/Research.jsx
@@ -2,9 +2,10 @@ import { useEffect, useState } from 'react';
 import addLogo from '../assets/add.svg'
 import { useNavigate  } from 'react-router-dom';
 import withAuthentication from '../HOCauth';
+import StudyComponent from '../components/StudyComponent';
 
 const Research = ({ userRoles }) =>  {
-    const navigate = useNavigate();
+    const navigate = useNavigate()
     const [studies, setStudies] = useState([]);
 
     useEffect(() => {
@@ -22,8 +23,8 @@ const Research = ({ userRoles }) =>  {
             return response.json();
         })
         .then(data => {
-            setStudies(data); // Update state with fetched data
-            console.log(studies)
+            setStudies(data); 
+            // console.log(studies)
         })
         .catch(error => {
             console.error('Error:', error);
@@ -37,6 +38,7 @@ const Research = ({ userRoles }) =>  {
     return (
         <>
             <div className="mx-3 my-20">
+                {/* SEARCH BAR */}
                 <div className="grid grid-col pb-4">
                     <div className="flex justify-center items-center bg-white transition duration-500 ease-in-out shadow-md hover:bg-gray-100 rounded-2xl hover:shadow-2xl">
                         <div className='w-full px-1'>
@@ -52,69 +54,37 @@ const Research = ({ userRoles }) =>  {
                     </div>
                 </div>
                 <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
-                    <div className="flex justify-center items-center bg-white transition duration-500 ease-in-out shadow-md hover:bg-gray-100 rounded-2xl hover:shadow-2xl">
-                        <div className='w-full px-1 md:px-2'>
-                            <div className='flex p-2 gap-2 justify-between'>
-                                <div className='flex-col'>
-                                    <h2 className="text-xl font-bold">STUDY NAME</h2>
-                                    <h3 className='italic font-mono'>Recruiter name</h3>
+                    {/* DISPLAYING STUDIES */}
+                    {studies.map((study, index) => (
+                        <div className="flex justify-center items-center bg-white transition duration-500 ease-in-out shadow-md hover:bg-gray-100 rounded-2xl hover:shadow-2xl">
+                            <div className='w-full px-1 md:px-2'>
+                                <div className='flex p-2 gap-2 justify-between'>
+                                    <div className='flex-col'>
+                                        <h2 className="text-xl font-bold">{study.name}</h2>
+                                        <h3 className='italic font-mono'>Recruiter name</h3>
+                                    </div>
+                                    <div className="text-right">
+                                        <h2>{study.category}</h2>
+                                    </div>
                                 </div>
-                                <div className="text-right">
-                                    <h2>Study Category</h2>
+                                <div className='flex px-2 pb-4 justify-between'>
+                                    <div className='flex-col'>
+                                        <p className='text-justify'>
+                                            {study.description}
+                                        </p>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className='flex px-2 pb-4 justify-between'>
-                                <div className='flex-col'>
-                                    <p className='text-justify'>
-                                        Bro wassup? What we doing out here?
-                                        What is this experiment we doin? We testing the 
-                                        donuts innit? Wassup with their mind cuh? What going
-                                        on up there in their minds? Testing dumbos. 
-                                    </p>
-                                </div>
-                            </div>
-                            <div className='flex mx-2 mb-2 text-white gap-2 text-center'>
-                                <div className='w-full bg-blue-500 p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-blue-600 transform hover:-translate-y-0.5'>
-                                    Edit
-                                </div>
-                                <div className='w-full bg-red-500  p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-red-800 transform hover:-translate-y-0.5'>
-                                    Delete
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex justify-center items-center bg-white transition duration-500 ease-in-out shadow-md hover:bg-gray-100 rounded-2xl hover:shadow-2xl">
-                        <div className='w-full px-1 md:px-2'>
-                            <div className='flex p-2 gap-2 justify-between'>
-                                <div className='flex-col'>
-                                    <h2 className="text-xl font-bold">STUDY NAME</h2>
-                                    <h3 className='italic font-mono'>Recruiter name</h3>
-                                </div>
-                                <div className="text-right">
-                                    <h2>Study Category</h2>
-                                </div>
-                            </div>
-                            <div className='flex px-2 pb-4 justify-between'>
-                                <div className='flex-col'>
-                                    <p className='text-justify'>
-                                        Bro wassup? What we doing out here?
-                                        What is this experiment we doin? We testing the 
-                                        donuts innit? Wassup with their mind cuh? What going
-                                        on up there in their minds? Testing dumbos. 
-                                    </p>
-                                </div>
-                            </div>
-                            <div className='flex mx-2 mb-2 text-white gap-2 text-center'>
-                                <div className='w-full bg-blue-500 p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-blue-600 transform hover:-translate-y-0.5'>
-                                    Edit
-                                </div>
-                                <div className='w-full bg-red-500  p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-red-800 transform hover:-translate-y-0.5'>
-                                    Delete
+                                <div className='flex mx-2 mb-2 text-white gap-2 text-center'>
+                                    <div className='w-full bg-blue-500 p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-blue-600 transform hover:-translate-y-0.5'>
+                                        Edit
+                                    </div>
+                                    <div className='w-full bg-red-500  p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-red-800 transform hover:-translate-y-0.5'>
+                                        Delete
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-
+                    ))}
                 </div>
             </div>
         </>

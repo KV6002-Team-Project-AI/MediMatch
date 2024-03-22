bff41c9a6fc9faff79f7e6a74f4fa5e2dec5d1fa
Syed W. Quadri <swq1999@gmail.com>
2024-03-22 23:22:22 +0000

    matches

---
+++ trapp/src/components/MinMaxTable.jsx
@@ -0,0 +1,95 @@
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function MinMaxTable(props) {
+
+    // Min processing
+    function minCheck(minVal){
+        if (minVal === 1) {
+            return "none";
+        } else {
+            return minVal;
+        }
+    }
+
+    // Max processing
+    function maxCheck(maxVal){
+        if (maxVal === 1 ||  maxVal === 120 || maxVal === 400) {
+            return "none";
+        } else {
+            return maxVal;
+        }
+    }
+
+    // Table styling
+    const StyledTableCell = styled(TableCell)(({ theme }) => ({
+        [`&.${tableCellClasses.head}`]: {
+          backgroundColor: '#9c9c9c',
+          color: theme.palette.common.white,
+        },
+        [`&.${tableCellClasses.body}`]: {
+          fontSize: 14,
+        },
+      }));
+      
+      const StyledTableRow = styled(TableRow)(({ theme }) => ({
+        '&:nth-of-type(odd)': {
+          backgroundColor: theme.palette.action.hover,
+        },
+        // hide last border
+        '&:last-child td, &:last-child th': {
+          border: 0,
+        },
+      }));
+
+    // Min-Max Table Creation
+    function createData(name, min, max) {
+        return { name, min, max };
+    }
+
+    // Creating Table Rows
+    const rows = [
+        createData('Age', minCheck(props.minAge), maxCheck(props.maxAge)),
+        createData('Weight (kg)', minCheck(props.minWeight),  maxCheck(props.maxWeight)),
+        createData('Height (cm)', minCheck(props.minHeight), maxCheck(props.maxHeight))
+    ];
+
+  return (
+    <>
+    {/* MIN MAX TABLE */}
+    <TableContainer component={Paper}>
+        <Table sx={{  }} size="small" aria-label="a dense table">
+            <TableHead>
+                <TableRow>
+                    <StyledTableCell><p className='font-semibold'>Attributes</p></StyledTableCell>
+                    <StyledTableCell align="right"><p className='font-semibold'>Min</p></StyledTableCell>
+                    <StyledTableCell align="right"><p className='font-semibold'>Max</p></StyledTableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+            {rows.map((row) => (
+                <StyledTableRow 
+                key={row.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                    <StyledTableCell  component="th" scope="row">
+                        <p className='font-semibold'>{row.name}</p>
+                    </StyledTableCell >
+                    <StyledTableCell  align="right">{row.min}</StyledTableCell >
+                    <StyledTableCell  align="right">{row.max}</StyledTableCell >
+                </StyledTableRow>
+            ))}
+            </TableBody>
+        </Table>
+        </TableContainer>
+    </>
+  );
+}
+
+export default MinMaxTable;
--- trapp/src/pages/Research.jsx
+++ trapp/src/pages/Research.jsx
@@ -2,13 +2,35 @@ import { useEffect, useState } from 'react';
 import addLogo from '../assets/add.svg'
 import { useNavigate  } from 'react-router-dom';
 import withAuthentication from '../HOCauth';
-import StudyComponent from '../components/StudyComponent';
+import EditNoteIcon from '@mui/icons-material/EditNote';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ZoomInIcon from '@mui/icons-material/ZoomIn';
+import Tooltip from '@mui/material/Tooltip';
+import Zoom from '@mui/material/Zoom';
+import MinMaxTable from '../components/MinMaxTable';
 
-import { ButtonContent, Button, Icon } from 'semantic-ui-react'
 
 const Research = ({ userRoles }) =>  {
     const navigate = useNavigate()
     const [studies, setStudies] = useState([]);
+
+    const tooltips = [
+        { title: "View", color: "blue", icon: <ZoomInIcon /> },
+        { title: "Edit", color: "gray", icon: <EditNoteIcon /> },
+        { title: "Delete", color: "red", icon: <DeleteIcon /> }
+    ];
+
+    // Data Format function
+    function formatDate(inputDate) {
+        const date = new Date(inputDate);
+        
+        // Get the day, month, and year components from the date object
+        const day = String(date.getDate()).padStart(2, '0');
+        const month = String(date.getMonth() + 1).padStart(2, '0'); // January is 0, so we add 1
+        const year = date.getFullYear();
+    
+        return `${day}-${month}-${year}`;
+    }
 
     useEffect(() => {
         fetch('http://localhost:8000/api/studycreate/', {
@@ -26,12 +48,12 @@ const Research = ({ userRoles }) =>  {
         })
         .then(data => {
             setStudies(data); 
-            // console.log(studies)
+            console.log(studies)
         })
         .catch(error => {
             console.error('Error:', error);
         });
-    }, []); // Empty dependency array ensures this effect runs only once when the component mounts
+    }, []); 
 
     if (!userRoles.is_recruiter && !userRoles.is_superuser) {
         return <div className='mt-20'>You do not have permission to view this page.</div>;
@@ -60,6 +82,18 @@ const Research = ({ userRoles }) =>  {
                     {studies.map((study, index) => (
                         <div className="flex justify-center items-center bg-white transition duration-500 ease-in-out shadow-md hover:bg-gray-100 rounded-2xl hover:shadow-2xl">
                             <div className='w-full px-1 md:px-2'>
+
+                                {/* DATES */}
+                                <div className='flex px-2 pb-2 mx-1 justify-center gap-2 pt-2 text-center items-center'>
+                                    <p className='w-full text-sm text-black rounded bg-emerald-200 py-0.5'>
+                                        <span className='font-semibold'>Starts:</span> {formatDate(study.start_date)}
+                                    </p>
+                                    <p className='w-full text-sm text-black rounded bg-red-200 py-0.5'>
+                                        <span className='font-semibold'>Expires:</span> {formatDate(study.expiry_date)}
+                                    </p>
+                                </div>
+
+                                {/* STUDY INFO CARD */}
                                 <div className='flex p-2 gap-2 justify-between'>
                                     <div className='flex-col'>
                                         <h2 className="text-xl font-bold">{study.name}</h2>
@@ -69,20 +103,63 @@ const Research = ({ userRoles }) =>  {
                                         <h2>{study.category}</h2>
                                     </div>
                                 </div>
-                                <div className='flex px-2 pb-4 justify-between'>
+                                <div className='flex px-2 pb-2 justify-between'>
                                     <div className='flex-col'>
                                         <p className='text-justify'>
                                             {study.description}
                                         </p>
                                     </div>
                                 </div>
-                                <div className='flex mx-2 mb-2 text-white gap-2 text-center'>
-                                    <div className='w-full bg-blue-500 p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-blue-600 transform hover:-translate-y-0.5'>
-                                        Edit
-                                    </div>
-                                    <div className='w-full bg-red-500  p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-red-800 transform hover:-translate-y-0.5'>
-                                        Delete
-                                    </div>
+                                
+                                {/* REQUIREMENTS */}
+                                <h1 className='text-center text-lg font-bold border-t-2 border-gray-300 py-2 mt-2 '>Requirements</h1>
+                                <div className='flex px-2 pb-4 justify-between'>
+                                    <MinMaxTable
+                                        minAge={study.min_age}
+                                        minWeight={study.min_weight}
+                                        minHeight={study.min_height}
+                                        maxAge={study.max_age}
+                                        maxWeight={study.max_weight}
+                                        maxHeight={study.max_height}
+                                    />
+                                </div>
+
+                                {/* PREFERENCES */}
+                                <h1 className='text-center text-lg font-bold mb-2 '>Preferences</h1>
+                                <div className='flex px-2 pb-4 justify-between'>
+                                    
+                                </div>
+
+                                {/* BUTTONS */}
+                                <div className='flex mx-2 mb-2 text-white gap-2 text-center'>                                                                         
+                                    {tooltips.map((tooltip, index) => (
+                                        <Tooltip 
+                                            key={index}
+                                            title={tooltip.title} 
+                                            placement="top" 
+                                            TransitionComponent={Zoom}
+                                            slotProps={{
+                                                popper: {
+                                                modifiers: [
+                                                    {
+                                                    name: 'offset',
+                                                    options: {
+                                                        offset: [0, -4],
+                                                    },
+                                                    },
+                                                ],
+                                                },
+                                            }}
+                                            arrow
+                                            disableInteractive
+                                            enterDelay={300} 
+                                            leaveDelay={200}
+                                        >
+                                            <div className={`w-full bg-${tooltip.color}-500 p-2 rounded-lg shadow hover:shadow-lg transition duration-300 ease-in-out hover:bg-${tooltip.color}-600 transform hover:-translate-y-0.5`}>
+                                                {tooltip.icon}
+                                            </div>
+                                        </Tooltip>
+                                    ))}
                                 </div>
                             </div>
                         </div>
